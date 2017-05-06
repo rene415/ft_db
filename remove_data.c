9b291f8b4809df470e873652dac5c8fd72de7b55
warren fung <wfung@e1z2r6p1.42.us.org>
2017-05-05 17:38:55 -0700

    made remove_data (it iteres to the specific row + col, frees ptr of struct variables, sets to null

--- remove_data.c
+++ remove_data.c
@@ -6,13 +6,38 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 17:02:56 by wfung             #+#    #+#             */
-/*   Updated: 2017/05/05 17:03:11 by wfung            ###   ########.fr       */
+/*   Updated: 2017/05/05 17:37:10 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_db.h"
 
-int		remove_data()
+void	remove_data(int row, int col, t_store *store)
 {
+	int		x;
+	int		y;
+	t_store	*buff;
 
+	x = 0;
+	y = 0;
+	while (store->next)
+	{
+		if (x == col - 1)
+		{
+			if (y == row - 1)
+			{
+				if (store)
+				{
+					free(store->str);
+					store->str = NULL;
+					free(store->x);
+					store->x = NULL;
+				}
+			}
+			x = 0;
+			y++;
+		}
+		x++;
+		store = store->next;
+	}
 }
