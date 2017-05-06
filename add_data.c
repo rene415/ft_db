28e426fdf3519cacd795770250052b63c4ff4da7
warren fung <wfung@e1z2r6p1.42.us.org>
2017-05-05 17:22:56 -0700

    moved lines from edit_data to add_data to separate functions

--- add_data.c
+++ add_data.c
@@ -6,13 +6,30 @@
 /*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/05 17:01:35 by wfung             #+#    #+#             */
-/*   Updated: 2017/05/05 17:01:53 by wfung            ###   ########.fr       */
+/*   Updated: 2017/05/05 17:22:13 by wfung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_db.h"
 
-int		add_data()
+t_store		*add_data(int row, int col )
 {
+	t_store 	*buff;
 
+	ft_putstr("Add Data\nPlease enter the number of Columns [Limit 10]\n");
+	scanf("%d", &col);
+	while (col > 10 || col < 1)
+	{
+		ft_putstr("Invalid Entry\nPlease enter the number of Columns [Limit 10]\n");
+		scanf("%d", &col);
+	}
+	ft_putstr("Add Data\nPlease enter the number of Rows [Limit 10]\n");
+	scanf("%d", &row);
+	while (row > 10 || row < 1)
+	{
+		ft_putstr("Invalid Entry. Please enter the number of Rows\n [Limit 10]\n");
+		scanf("%d", &row);
+	}
+	buff = create_store(row, col);
+	return (0);
 }
